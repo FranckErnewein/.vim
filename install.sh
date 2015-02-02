6fdfac80be1b3f13a6c38ebb719637b33cfe4acd
Franck Ernewein <franck.ernewein@gmail.com>
2015-02-02 18:16:01 +0100

    include default .editorconfig

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #Create symlinks:
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ~/.vim/.editorconfig ~/.editorconfig
 
 #Switch to the `~/.vim` directory, and fetch submodules:
 cd ~/.vim
